642b6ae72b670b98a0721f688e1c3e1d5760f5a3
karol.wolny <revodev500@gmail.com>
2023-01-15 15:33:51 +0100

    Fix Plugin.java

--- src/main/java/com/revo/skyblock/Plugin.java
+++ src/main/java/com/revo/skyblock/Plugin.java
@@ -3,7 +3,6 @@ package com.revo.skyblock;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.revo.skyblock.application.ProvideModule;
-import com.revo.skyblock.command.TestExecutor;
 import com.revo.skyblock.listener.SavePlayerListener;
 import com.revo.skyblock.repository.file.FileManager;
 import com.revo.skyblock.world.WorldManager;
